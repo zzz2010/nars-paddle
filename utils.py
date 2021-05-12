e3054ed7605e04512b009903951d57f04d095997
littledesk <zzz2010@gmail.com>
2021-05-11 21:04:55 -0400

    fix import

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-import torch
+import paddorch as torch
 import numpy as np
 
 
